bb60287c7e358da79d9e38eacf1ea39b40504464
Sahil Shaikh <sahilshaikh8264@gmail.com>
2025-02-08 22:10:58 +0000

    added config to allow non-https backend for testing purpose

--- next.config.js
+++ next.config.js
@@ -4,3 +4,22 @@ const nextConfig = {
 }
 
 module.exports = nextConfig 
+
+module.exports = {
+  images: {
+    domains: ['13.48.10.137'], // Add your API domain/IP
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests",
+          },
+        ],
+      },
+    ];
+  },
+};
